1a8a62359ad715f71ecc27d79e83c7eef82313aa
Rui Maciel <rui.maciel@gmail.com>
2017-08-10 01:05:45 +0100

    tests: adds common tests to test_elements

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -3,7 +3,8 @@ import unittest
 from barman import materials, dofs, sections
 from barman.elements import Bar2, EulerBernoulli
 
-class TestBar2Methods(unittest.TestCase):
+
+class CommonTests:
 
     def setUp(self):
         self.nodes = [ dofs.Node([0,0]), dofs.Node([2,0]) ]
@@ -11,59 +12,89 @@ class TestBar2Methods(unittest.TestCase):
         self.section = sections.Section(1, 1);
         
 
+    def test_get_local_stiffness_matrix_expect_square_matrix(self):
+        
+        element = self.getElement()
+        k = element.get_local_stiffness_matrix()
+
+        k_rows, k_columns = k.shape
+
+        self.assertEqual(k_rows, k_columns)
+
+
+    def test_get_global_stiffness_matrix_expect_square_matrix(self):
+        
+        element = self.getElement()
+        k = element.get_global_stiffness_matrix()
+
+        k_rows, k_columns = k.shape
+
+        self.assertEqual(k_rows, k_columns)
+
+
+    def test_get_global_stiffness_matrix_expect_matrix_with_n_dof_rows(self):
+
+        element = self.getElement()
+        k = element.get_global_stiffness_matrix()
+
+        k_rows, k_columns = k.shape
+        n_dofs = len(element.get_global_dofs())
+
+        self.assertEqual(k_rows, n_dofs)
+
+
+
+class TestBar2Methods(CommonTests, unittest.TestCase):
+
+    def getElement(self):
+        return Bar2(self.nodes, self.section, self.material)
+
+
     def test_get_global_dofs_length(self):
     
-        element = Bar2(self.nodes, self.section, self.material)
+        element = self.getElement()
         global_dofs = element.get_global_dofs()
 
         self.assertEqual(len(global_dofs), 4)
 
 
     def test_get_bar_length(self):
-        element = Bar2(self.nodes, self.section, self.material)
+
+        element = self.getElement()
         bar_length = element.get_length()
 
         self.assertEqual(bar_length, 2)
         
 
-    def test_get_local_stiffness_matrix(self):
-        element = Bar2(self.nodes, self.section, self.material)
-        k = element.get_local_stiffness_matrix()
-
-
     def test_get_transformation_matrix(self):
-        element = Bar2(self.nodes, self.section, self.material)
+
+        element = self.getElement()
+        
         T = element.get_transformation_matrix()
 
 
-class TestEulerBernoulliMethods(unittest.TestCase):
 
-    def setUp(self):
-        self.nodes = [ dofs.Node([0,0]), dofs.Node([2,0]) ]
-        self.material = materials.LinearElastic('test', 100, 0.35);
-        self.section = sections.Section(1, 1);
+class TestEulerBernoulliMethods(CommonTests, unittest.TestCase):
+
+    def getElement(self):
+        return EulerBernoulli(self.nodes, self.section, self.material)
         
 
     def test_get_global_dofs_length(self):
     
-        element = EulerBernoulli(self.nodes, self.section, self.material)
+        element = self.getElement()
         global_dofs = element.get_global_dofs()
 
         self.assertEqual(len(global_dofs), 6)
 
 
     def test_get_bar_length(self):
-        element = EulerBernoulli(self.nodes, self.section, self.material)
+
+        element = self.getElement()
         bar_length = element.get_length()
 
         self.assertEqual(bar_length, 2)
         
-
-    def test_get_local_stiffness_matrix(self):
-        element = EulerBernoulli(self.nodes, self.section, self.material)
-        k = element.get_local_stiffness_matrix()
-        
-
 
 if __name__ == '__main__':
     unittest.main()
