f288eb7d4141cc31836206d1c4911749ad3cb84c
Rui Maciel <rui.maciel@gmail.com>
2017-08-29 22:49:42 +0100

    creates test for linear static analysis

--- Makefile
+++ Makefile
@@ -5,3 +5,4 @@ test:
 	python3 -m unittest tests/test_dofs.py
 	python3 -m unittest tests/test_elements.py
 	python3 -m unittest tests/test_models_linear_elastic.py
+	python3 -m unittest tests/test_analysis_LinearStatic.py
--- tests/test_models_linear_elastic.py
+++ tests/test_models_linear_elastic.py
@@ -1,7 +1,6 @@
 import unittest
 
 from barman import models
-from barman.analysis import LinearStatic
 from barman import materials, dofs, sections
 from barman.prescribed_displacements import PrescribedDisplacement
 from barman.elements import Bar2, EulerBernoulli
@@ -23,66 +22,6 @@ class TestLinearElasticMethods(unittest.TestCase):
         elem = model.elements
 
         self.assertEqual(len(elem), 1)
-
-
-    def test_get_global_dof_map(self):
-        """ tests the generation of the GlobalDoF map"""
-
-        model = models.LinearStatic()
-
-        elem = Bar2([self.nodes[0], self.nodes[1]], self.section, self.material)
-
-        model.append_element(elem)
-
-        # now, let's set the beam as a singly-supported
-        model.append_prescribed_displacement( PrescribedDisplacement( dofs.GlobalDoF(self.nodes[0], dofs.Parameter.dx), 0))
-        model.append_prescribed_displacement( PrescribedDisplacement( dofs.GlobalDoF(self.nodes[0], dofs.Parameter.dy), 0))
-        model.append_prescribed_displacement( PrescribedDisplacement( dofs.GlobalDoF(self.nodes[1], dofs.Parameter.dy), 0))
-
-
-        analysis = LinearStatic()
-
-        global_dofs, essential_global_dofs = analysis.get_global_dof_map(model)
-        
-        self.assertEqual(len(global_dofs), 4)
-    
-
-    def test_generate_global_dof_vector(self):
-        """ tests the generation of the GlobalDoF map"""
-
-        model = models.LinearStatic()
-
-        elem = Bar2([self.nodes[0], self.nodes[1]], self.section, self.material)
-
-        model.append_element(elem)
-
-        # now, let's set the beam as a singly-supported
-        model.append_prescribed_displacement( PrescribedDisplacement( dofs.GlobalDoF(self.nodes[0], dofs.Parameter.dx), 0))
-        model.append_prescribed_displacement( PrescribedDisplacement( dofs.GlobalDoF(self.nodes[0], dofs.Parameter.dy), 0))
-        model.append_prescribed_displacement( PrescribedDisplacement( dofs.GlobalDoF(self.nodes[1], dofs.Parameter.dy), 0))
-
-        analysis = LinearStatic()
-        global_dof_map = analysis.get_global_dof_map(model);
-        global_dof_map, essential_global_dofs = analysis.get_global_dof_map(model)
-
-        global_dofs = analysis.generate_global_dof_vector(model.prescribed_displacements, global_dof_map)
-        
-        self.assertEqual(len(global_dofs), 4)
-    
-
-    def test_run(self):
-        model = models.LinearStatic()
-        model.append_element( Bar2([self.nodes[0], self.nodes[1]], self.section, self.material) )
-        model.append_element( Bar2([self.nodes[1], self.nodes[2]], self.section, self.material) )
-        model.append_element( Bar2([self.nodes[0], self.nodes[2]], self.section, self.material) )
-
-        model.append_prescribed_displacement( PrescribedDisplacement( dofs.GlobalDoF(self.nodes[0], dofs.Parameter.dx), 0))
-        model.append_prescribed_displacement( PrescribedDisplacement( dofs.GlobalDoF(self.nodes[0], dofs.Parameter.dy), 0))
-        model.append_prescribed_displacement( PrescribedDisplacement( dofs.GlobalDoF(self.nodes[1], dofs.Parameter.dx), 0))
-        model.append_prescribed_displacement( PrescribedDisplacement( dofs.GlobalDoF(self.nodes[1], dofs.Parameter.dy), 0))
-
-        analysis = LinearStatic()
-        result = analysis.run(model)
 
 
 if __name__ == '__main__':
