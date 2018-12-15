bdb295f670b7642d9d6360f98219924b5188ce04
Rui Maciel <rui.maciel@gmail.com>
2018-12-15 16:40:26 +0000

    Bumps version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='barman',
-    version='1.0.0',
+    version='1.0.1dev',
     description='A 2D engineering beam theory model implemente in Python 3',
     packages=['barman'],
     author='Rui Maciel',
